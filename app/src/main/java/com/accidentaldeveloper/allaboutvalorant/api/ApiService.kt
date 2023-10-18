039ed900c4f153503d7b876971c09dabad4bf315
vinit <vinithanabar20@gmail.com>
2023-10-18 19:43:29 +0530

    Built_Api_Interface

--- app/src/main/java/com/accidentaldeveloper/allaboutvalorant/api/ApiService.kt
+++ app/src/main/java/com/accidentaldeveloper/allaboutvalorant/api/ApiService.kt
@@ -1,12 +1,31 @@
 package com.accidentaldeveloper.allaboutvalorant.api
 
+import com.accidentaldeveloper.allaboutvalorant.helper.AppConstant
+import com.accidentaldeveloper.allaboutvalorant.models.agent_model.Agents_Response
+import com.accidentaldeveloper.allaboutvalorant.models.bundle_model.Bundle_Response
+import com.accidentaldeveloper.allaboutvalorant.models.maps_model.Maps_Response
+import com.accidentaldeveloper.allaboutvalorant.models.spray_model.Spary_Response
+import com.accidentaldeveloper.allaboutvalorant.models.weapons_model.Weapon_Response
 import retrofit2.Call
+import retrofit2.http.GET
 
 interface ApiService {
-    /* const val AGENT_END_POINT = "/agents"
-    const val BUNDLES_END_POINT = "/bundles"
-    const val MAPS_END_POINT = "/maps"
-    const val WEAPONS_END_POINT = "/weapons"
-    const val SPRAY_END_POINT = "/sprays"*/
+
+    @GET(AppConstant.AGENT_END_POINT)
+    suspend fun getAgents():Call<Agents_Response>
+
+    @GET(AppConstant.BUNDLES_END_POINT)
+    suspend fun getBundels():Call<Bundle_Response>
+
+    @GET(AppConstant.AGENT_END_POINT)
+    suspend fun getMaps():Call<Maps_Response>
+
+    @GET(AppConstant.WEAPONS_END_POINT)
+    suspend fun getWeapons():Call<Weapon_Response>
+
+    @GET(AppConstant.SPRAY_END_POINT)
+    suspend fun getSpray():Call<Spary_Response>
+
+
 
 }
