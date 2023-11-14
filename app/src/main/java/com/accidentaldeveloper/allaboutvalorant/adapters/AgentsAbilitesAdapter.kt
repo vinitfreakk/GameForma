0f9e448899c31e5ab06f7f14dc9f18f879986075
vinit <vinithanabar20@gmail.com>
2023-11-14 22:06:52 +0530

    made adapter for abilites

--- app/src/main/java/com/accidentaldeveloper/allaboutvalorant/adapters/AgentsAbilitesAdapter.kt
+++ app/src/main/java/com/accidentaldeveloper/allaboutvalorant/adapters/AgentsAbilitesAdapter.kt
@@ -1,4 +1,40 @@
 package com.accidentaldeveloper.allaboutvalorant.adapters
 
-class AgentsAbilitesAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.accidentaldeveloper.allaboutvalorant.R
+import com.accidentaldeveloper.allaboutvalorant.models.Valorant_Model.agent_model.Ability
+import com.bumptech.glide.Glide
+
+class AgentsAbilitesAdapter(val abilites: List<Ability>) :
+    RecyclerView.Adapter<AgentsAbilitesAdapter.myAgents_Abilites_ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myAgents_Abilites_ViewHolder {
+       val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.agents_abilites_rv_items,parent,false)
+        return myAgents_Abilites_ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: myAgents_Abilites_ViewHolder, position: Int) {
+        holder.abilites_txt.text = abilites[position].displayName
+        holder.abilitesInfo.text = abilites[position].description
+       Glide.with(holder.abiliteImage).load(abilites[position].displayIcon).into(holder.abiliteImage)
+    }
+
+    override fun getItemCount(): Int {
+       return abilites.size
+    }
+
+    class myAgents_Abilites_ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+       val abilites_txt = itemView.findViewById<TextView>(R.id.agent_ablites_name)
+        val abilitesInfo  = itemView.findViewById<TextView>(R.id.agent_abilities_data)
+        val abiliteImage = itemView.findViewById<ImageView>(R.id.ablites_img)
+    }
 }
+
+
+
+
