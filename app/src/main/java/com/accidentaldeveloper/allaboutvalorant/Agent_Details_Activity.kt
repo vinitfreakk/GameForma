134077d4845cfca0e4a56bfd141855b8472bae49
vinit <vinithanabar20@gmail.com>
2023-11-14 22:28:21 +0530

    bought the data from api and made it visible in rv

--- app/src/main/java/com/accidentaldeveloper/allaboutvalorant/Agent_Details_Activity.kt
+++ app/src/main/java/com/accidentaldeveloper/allaboutvalorant/Agent_Details_Activity.kt
@@ -6,20 +6,31 @@ import android.util.Log
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.activity.viewModels
 import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.accidentaldeveloper.allaboutvalorant.adapters.AgentsAbilitesAdapter
 import com.accidentaldeveloper.allaboutvalorant.databinding.ActivityAgentDetailsBinding
 import com.accidentaldeveloper.allaboutvalorant.databinding.ActivityMainBinding
+import com.accidentaldeveloper.allaboutvalorant.models.Valorant_Model.agent_model.Ability
 import com.accidentaldeveloper.allaboutvalorant.models.adapterModel.AgentsAdapterModel
+import com.accidentaldeveloper.allaboutvalorant.viewmodel.AgentsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
 
+@AndroidEntryPoint
 class Agent_Details_Activity : AppCompatActivity() {
     lateinit var binding:ActivityAgentDetailsBinding
+    lateinit var recyclerView: RecyclerView
+    private val viewmodel:AgentsViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgentDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val agentData = intent.getSerializableExtra("AGENT_DATA") as? AgentsAdapterModel
-        val agentIndex = intent?.getIntExtra("AgentIndex",0)
+        val agentIndex = intent.getIntExtra("AgentIndex",0)
         Log.d("agentIndex", "onCreate: ${agentIndex}")
 
         val agentImageView = binding.agentimg
@@ -28,6 +39,15 @@ class Agent_Details_Activity : AppCompatActivity() {
         agentImageView.startAnimation(AnimationUtils.loadAnimation(this@Agent_Details_Activity,R.anim.fade_in))
         binding.agentName.text = agentData?.agentName ?: "omen"
         binding.agentType.text = agentData?.agentType ?: "omen"
+
+        recyclerView = binding.agentPowerRv
+       viewmodel.liveData.observe(this,{
+           val response = it.data.get(agentIndex).abilities
+           binding.agentBio.text = it.data.get(agentIndex).description
+           val list = response as ArrayList<Ability>
+           recyclerView.adapter = AgentsAbilitesAdapter(list)
+           recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+       })
 
     }
 }
