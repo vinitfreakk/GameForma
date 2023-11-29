46a40d219aeee0b2cf50b15ad24364032ad36eeb
vinit <vinithanabar20@gmail.com>
2023-11-29 11:17:53 +0530

    done with app bugs needs to be fixed

--- app/src/main/java/com/accidentaldeveloper/allaboutvalorant/adapters/GamesAdapter.kt
+++ app/src/main/java/com/accidentaldeveloper/allaboutvalorant/adapters/GamesAdapter.kt
@@ -1,10 +1,13 @@
 package com.accidentaldeveloper.allaboutvalorant.adapters
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.accidentaldeveloper.allaboutvalorant.R
 import com.accidentaldeveloper.allaboutvalorant.models.MMO_Models.Games.Game_ResponseItem
@@ -24,6 +27,15 @@ class GamesAdapter(private val gamelist: List<Game_ResponseItem>) :
       Glide.with(holder.game_img).load(gamelist[position].thumbnail).into(holder.game_img)
         holder.game_name.text = gamelist[position].title
         holder.game_paltform.text = "Platform:${gamelist[position].platform}"
+        holder.games_card.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(gamelist[position].gameUrl))
+            try {
+                holder.games_card.context.startActivity(intent)
+            } catch (e: android.content.ActivityNotFoundException) {
+                intent.data = Uri.parse(gamelist[position].gameUrl)
+                holder.games_card.context.startActivity(intent)
+            }
+        }
     }
 
 
@@ -36,6 +48,7 @@ class GamesAdapter(private val gamelist: List<Game_ResponseItem>) :
         val game_img = itemView.findViewById<ImageView>(R.id.game_img)
         val game_name = itemView.findViewById<TextView>(R.id.game_name)
         val game_paltform = itemView.findViewById<TextView>(R.id.game_paltfrom)
+        val games_card = itemView.findViewById<CardView>(R.id.games_card)
     }
 
 }
