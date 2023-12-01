353ab681119ab937a897c9a148cc5d1fb2a66934
vinit <vinithanabar20@gmail.com>
2023-12-01 08:07:52 +0530

    added network class

---
+++ app/src/main/java/com/accidentaldeveloper/allaboutvalorant/helper/NetworkManager.kt
@@ -0,0 +1,55 @@
+package com.accidentaldeveloper.allaboutvalorant.helper
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.lifecycle.LiveData
+
+class NetworkManager(context: Context):LiveData<Boolean>() {
+
+    override fun onActive() {
+        super.onActive()
+        checkNetworkConnectivity()
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    private var connectivityManager = context.applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback(){
+
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            postValue(true)
+        }
+
+        override fun onUnavailable() {
+            super.onUnavailable()
+            postValue(false)
+        }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            postValue(false)
+        }
+    }
+
+    private fun checkNetworkConnectivity(){
+        val network = connectivityManager.activeNetwork
+        if(network==null) postValue(false)
+        val requestBuilder = NetworkRequest.Builder().apply {
+            addCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+            addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET)
+            addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        }.build()
+
+       connectivityManager.registerNetworkCallback(requestBuilder,networkCallback)
+
+    }
+}
